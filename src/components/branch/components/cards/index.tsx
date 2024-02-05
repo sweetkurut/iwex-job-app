67c2154dbed5e9f4f7bf29952881c2ee41c8ee75
sweetkurut <devxwc@gmail.com>
2024-02-05 17:20:52 +0600

    branches

---
+++ src/components/branch/components/cards/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./cards.module.sass";
+
+const Cards = () => {
+  return <div>Cards</div>;
+};
+
+export default Cards;
--- src/components/branch/index.tsx
+++ src/components/branch/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "./branch.module.sass";
 
 const Branches = () => {
-  return <div>Branches</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Филиалы</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Branches;
