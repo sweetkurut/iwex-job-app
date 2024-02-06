38166f9e30f2d06aa1231c168a2b327273d90529
sweetkurut <devxwc@gmail.com>
2024-02-06 10:24:17 +0600

    removed header and footer on login page

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,41 +1,37 @@
-
 import Home from "../../pages/home/Home";
 import Vacancies from "../../pages/vacancies/vacancies";
 import PageResponse from "../../pages/response/response";
 import Branch from "../../pages/branch/branch";
 import PageDetailVacancies from "../../pages/detailvacancies";
 import PagePrivacy from "../../pages/privacypolicy/privacy";
-import { Navigate, Route, Routes } from 'react-router-dom';
-import Login from '../../pages/login/Login';
+import { Navigate, Route, Routes } from "react-router-dom";
+import Login from "../../pages/login/Login";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { getCookie } from '../../utils/js_cookie';
+import { getCookie } from "../../utils/js_cookie";
 const Main = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const token = getCookie('accessToken');
-        if (token) {
-            <Navigate to="/" />
-        }
-    }, [dispatch]);
+  useEffect(() => {
+    const token = getCookie("accessToken");
+    if (token) {
+      <Navigate to="/" />;
+    }
+  }, [dispatch]);
 
-
-    return (
-        <main>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path='login' element={<Login />} />
-                <Route path="/vacancies" element={<Vacancies />} />
-                <Route path="/privacy" element={<PagePrivacy />} />
-                <Route path="/response" element={<PageResponse />} />
-                <Route path="/branch" element={<Branch />} />
-                <Route path="/card-detail-vacancies" element={<PageDetailVacancies />} />
-            </Routes>
-        </main>
-    );
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="/vacancies" element={<Vacancies />} />
+        <Route path="/privacy" element={<PagePrivacy />} />
+        <Route path="/response" element={<PageResponse />} />
+        <Route path="/branch" element={<Branch />} />
+        <Route path="/card-detail-vacancies" element={<PageDetailVacancies />} />
+      </Routes>
+    </main>
+  );
 };
 
-
 export default Main;
-
