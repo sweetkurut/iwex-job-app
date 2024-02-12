9fb8dcda8d3ebe1a3f7528aca50c4b777d147500
Wi-l1a <borzenko605@gmail.com>
2024-02-12 09:49:49 +0600

    .

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,23 +1,21 @@
+import Home from "../../pages/home/Home";
 import Vacancies from "../../pages/vacancies/vacancies";
 import PageResponse from "../../pages/response/response";
+import Branch from "../../pages/branch/branch";
 import PageDetailVacancies from "../../pages/detailvacancies";
 import PagePrivacy from "../../pages/privacypolicy/privacy";
 import { Route, Routes } from "react-router-dom";
 import Login from "../../pages/login/Login";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { getCookie } from "../../utils/js_cookie";
-import { useNavigate } from "react-router-dom";
+import { getCookie } from '../../utils/js_cookie';
+import { useNavigate } from 'react-router-dom';
 import Profile from "../../pages/profile/Profile";
 import { getDataProfile } from "../../store/slices/companyDetailsSlice";
 import Page404 from "../../pages/404/404";
 import EditVacancyPage from "../../pages/editVacancies/edit";
-import AddVacancy from "../vacancies/components/addVacancy/AddVacancy";
-import Branch from "../../pages/profile/components/branch/Branch";
-import Home from "../home";
-
 const Main = () => {
-    const dispatch = useDispatch();
+    const dispatch = useDispatch()
     const navigate = useNavigate();
     const token = getCookie("accessToken");
 
@@ -35,11 +33,11 @@ const Main = () => {
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/profile" element={<Profile />} />
-                <Route path="/" element={<Vacancies />} />
-                <Route path="/add-vacancy" element={<AddVacancy />} />
+                <Route path="/vacancies" element={<Vacancies />} />
                 <Route path="/privacy" element={<PagePrivacy />} />
                 <Route path="/response" element={<PageResponse />} />
-                <Route path="/card-detail-vacancies/:id" element={<PageDetailVacancies />} />
+                <Route path="/branch" element={<Branch />} />
+                <Route path="/card-detail-vacancies" element={<PageDetailVacancies />} />
                 <Route path="/edit-detail-vacancy/:id" element={<EditVacancyPage />} />
                 <Route path="*" element={<Page404 />} />
                 {/* Add a catch-all route to redirect to home or handle not found pages */}
